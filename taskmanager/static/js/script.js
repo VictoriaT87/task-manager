45fedb53f6ecbb1064e255522d7fdcfca269352f
VictoriaT87 <victoriaemt@gmail.com>
2023-02-02 09:48:15 +0000

    Add tasks view to frontend

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -19,4 +19,8 @@ document.addEventListener("DOMContentLoaded", function() {
     // select initialization
     let selects = document.querySelectorAll('select');
     M.FormSelect.init(selects);
+
+    // collapsibles
+    var collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
 });
